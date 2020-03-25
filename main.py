61f6fc914be015fd67427b16d93f85943ba70013
Vincent Slingsby <mironegaines@gmail.com>
2020-03-25 11:24:50 -0700

    ASCII art, console messages

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ from waterLevel import WaterLevel
 
 #https://codereview.stackexchange.com/questions/238105/interactive-discord-bot-for-tabletop-rpg
 
+print(
+'''
+  _____ _______          _______
+ |_   _|  __ \ \        / / ____|
+   | | | |__) \ \  /\  / / (___
+   | | |  ___/ \ \/  \/ / \___ \
+  _| |_| |      \  /\  /  ____) |
+ |_____|_|       \/  \/  |_____/
+  IoT  Plant    Watering  System
+'''
+)
 #configure MQTT connection
+print("Loading Config...")
 config = {}
 config['host']
 config['port'] = "8883"
@@ -18,29 +30,42 @@ pumpPin = 20
 moisturePin = 20
 
 #create objects
+print("Creating Objects...")
 moisture = Moisture(moisturePin)
 pump = Pump(pumpPin)
 notify = Notify(config)
 waterLevel = WaterLevel(0,0,True)
 
 #test modules
+print("Testing Modules...")
 moisture.test()
+waterLevel.test()
+
+#set water level
+waterLevel.set()
 
 #connect to AWS
+print("Connecting to AWS...")
 try:
     notify.connect()
 except:
     raise
 #main loop
+print("Beginning to monitor soil moisture.")
 try:
     while True:
         if (moisture.isDry()):
+            print("Soil is dry, sending notification.")
             notify.notifyDry()
-            if notify.pumpIsEnabled():
+            if notify.pumpIsEnabled() and not waterLevel.waterIsLow():
+                print("Sending notification and turning on pump.")
                 notify.notifyWatering()
                 pump.pumpForSeconds(1)
+                print("Turning off pump and sleeping.")
+
         else: #not dry, all is good
-            sleep(60)
+            print("Soil is moist.")
+        sleep(60)
 except KeyboardInterrupt:
     GPIO.cleanup()
 except:
