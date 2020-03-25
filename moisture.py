66781554120bfbb10804981c90e827164af1443b
Vincent Slingsby <mironegaines@gmail.com>
2020-03-25 08:29:02 -0700

    Test makes sure sensor is reading somewhat consistently, read() is fast

--- moisture.py
+++ moisture.py
@@ -10,20 +10,13 @@ class Moisture:
 
     def test(self):
         try:
-            GPIO.setmode(GPIO.BCM)  # for GPIO pin numbering
-            GPIO.setwarnings(False)  # disable warnings
-            # set button pin to an input port and set to pull-down mode
-            GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-            flag = False
+            count = 0
             for x in range(10):
-                if (read() != None and flag != True):
-                    flag = True
-
-            GPIO.cleanup()
-            return flag
+                if (read() != None):
+                    count++
+            if (count > 5):
+                return True
         except Exception as e:
-            GPIO.cleanup()
             return False
 
     def read(self):
@@ -45,7 +38,7 @@ class Moisture:
         for x in range(9):
             if read():
                 count++
-            time.sleep(1)
+            time.sleep(0.05)
         if (count > 7):
             return True
         else return False
